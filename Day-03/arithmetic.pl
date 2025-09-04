dbdc237c6eb0d5221d8db66afacc063a1e987986
Fathima Azha <2021ict48@stu.vau.ac.lk>
2025-09-04 12:36:09 +0530

    Update arithmetic.pl

--- Day-03/arithmetic.pl
+++ Day-03/arithmetic.pl
@@ -29,5 +29,10 @@ output_values(First, Last):- First=\=Last, write(First),
 
 
 %6
+sumto(1,1).
+sumto(N,S):- N>1, N1 is N-1, sumto(N1, S1),S is S1+N.
 
+%7
+writesquares(1):- write(1),nl.
+writesquares(N) :-  N>1, N1 is N-1,writesquares(N1), Nsq is N*N, write(Nsq), nl.
 
